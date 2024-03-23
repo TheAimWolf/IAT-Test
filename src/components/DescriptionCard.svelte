2938ce008f1870a20119f7f2afe5662cdd07d011
TheAimWolf <60583716+TheAimWolf@users.noreply.github.com>
2024-03-23 15:49:43 +0100

    Hotfix mobile input wrong

--- src/components/DescriptionCard.svelte
+++ src/components/DescriptionCard.svelte
@@ -1,52 +1,57 @@
-<script lang='ts'>
-    import { Label, Input, Button, Helper} from 'flowbite-svelte';
-    let userAge:any;
-    let error:boolean = false;
-    
-    const takeTest = () => {
-        let tryUserAge = Number(userAge)
-        if (typeof tryUserAge === 'number' && userAge > 0 && userAge <= 110) {
-            curUserAge = tryUserAge;
-            curState = 'test';
-        } else {
-            error = true;
-        }
-    }
-    export let curTestInfo:any, curState:string, curUserAge:number;
-    
+<script lang="ts">
+	import { Label, Input, Button, Helper } from 'flowbite-svelte';
+	let userAge: any;
+	let error: boolean = false;
+
+	const takeTest = () => {
+		let tryUserAge = Number(userAge);
+		if (typeof tryUserAge === 'number' && userAge > 0 && userAge <= 110) {
+			curUserAge = tryUserAge;
+			curState = 'test';
+		} else {
+			error = true;
+		}
+	};
+	export let curTestInfo: any, curState: string, curUserAge: number;
 </script>
 
-<div class="w-full h-full flex flex-col items-center">
-    <div class="flex justify-center w-full h-full">
-        <div class="bg-slate-500 rounded-lg border-2 w-full h-full">
-            {#if curTestInfo}
-            <div class="flex flex-col">
-                <div class="flex justify-between">
-                        <h1 class="m-4 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{curTestInfo.label}</h1>
-                    </div>
-                </div>
-                <div class="flex h-4/5 w-full flex-col items-center justify-center overflow-hidden">
-                    <div class="flex h-4/5 w-8/12 flex-col items-center justify-center">
-                        <h1 class="m-4 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                            <p>{curTestInfo.description}</p>
-                        </h1>
-                        <div class="mb-6">
-                            <div class="mb-6">
-                                <Label for='age' color='gray' class='block mb-2'>Dein Alter</Label>
-                                <Input id='age' color='base' placeholder="21" bind:value={userAge} />
-                                {#if error}
-                                <Helper class='mt-2' color='red'><span class="font-medium">Fehler</span> Bitte gebe eine gültiges Alter ein</Helper> 
-                                {/if}
-                            </div>
-                        </div>
-                    <div>
-                        <Button on:click={takeTest}>Test starten</Button>
-                    </div>
-                </div>
-            </div>
-            {:else}
-                <h1 class="m-4 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Loading...</h1>
-            {/if}
-        </div>
-    </div>
+<div class="flex h-full w-full flex-col items-center">
+	<div class="flex h-full w-full justify-center">
+		<div class="h-full w-full rounded-lg border-2 bg-slate-500">
+			{#if curTestInfo}
+				<div class="flex flex-col">
+					<div class="flex justify-between">
+						<h1 class="m-4 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+							{curTestInfo.label}
+						</h1>
+					</div>
+				</div>
+				<div class="flex h-4/5 w-full flex-col items-center justify-center">
+					<div class="flex h-4/5 w-8/12 flex-col items-center justify-center">
+						<h1 class="m-4 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+							<p>{curTestInfo.description}</p>
+						</h1>
+						<div class="mb-6">
+							<div class="mb-6">
+								<Label for="age" color="gray" class="mb-2 block">Dein Alter</Label>
+								<Input id="age" color="base" placeholder="21" bind:value={userAge} />
+								{#if error}
+									<Helper class="mt-2" color="red"
+										><span class="font-medium">Fehler</span> Bitte gebe eine gültiges Alter ein</Helper
+									>
+								{/if}
+							</div>
+						</div>
+						<div>
+							<Button on:click={takeTest}>Test starten</Button>
+						</div>
+					</div>
+				</div>
+			{:else}
+				<h1 class="m-4 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+					Loading...
+				</h1>
+			{/if}
+		</div>
+	</div>
 </div>
--- src/routes/take-test/+page.svelte
+++ src/routes/take-test/+page.svelte
@@ -11,8 +11,8 @@
 	const picturePath = `src/sets/${testName}/pictures.json`;
 	const testInfoPath = `src/sets/${testName}/testinfo.json`;
 
-	let inQuestionPause:boolean = false;
-	let wrongAnswer:boolean = false;
+	let inQuestionPause: boolean = false;
+	let wrongAnswer: boolean = false;
 
 	let lastTimestamp: number;
 
@@ -22,8 +22,11 @@
 		aGood: boolean; //If set a was asosiatet with the good words -> true
 		constructor(pCorrect: boolean, pAGood: boolean, pTime?: number) {
 			let now = Date.now();
-			if(pTime){this.time = pTime}
-			else{this.time = now - lastTimestamp;}
+			if (pTime) {
+				this.time = pTime;
+			} else {
+				this.time = now - lastTimestamp;
+			}
 			this.correct = pCorrect;
 			this.aGood = pAGood;
 		}
@@ -94,10 +97,18 @@
 		}
 	}
 
-	async function showResults(testResults:testEntry[]) {
-		const dScore:number = calculateDScore(testResults);
-		if(dScore === -1000){testResultString = 'Leider hast du zu viele Fehleingaben gemacht, weswegen der Test nicht ausgewertet werden kann.'; return;}
-		if(dScore === -1010){testResultString = 'Du bist leider zu schnell gewesen. Schonmal über eine E-Sports Karriere nachgedacht?'; return;}
+	async function showResults(testResults: testEntry[]) {
+		const dScore: number = calculateDScore(testResults);
+		if (dScore === -1000) {
+			testResultString =
+				'Leider hast du zu viele Fehleingaben gemacht, weswegen der Test nicht ausgewertet werden kann.';
+			return;
+		}
+		if (dScore === -1010) {
+			testResultString =
+				'Du bist leider zu schnell gewesen. Schonmal über eine E-Sports Karriere nachgedacht?';
+			return;
+		}
 		fetch('/save', {
 			method: 'PUT',
 			body: JSON.stringify({
@@ -106,12 +117,18 @@
 				age: userAge,
 				testType: 'prod'
 			})
-		})
-		if(dScore <= -1){testResultString = testInfo['b-full']}
-		else if (dScore > -1 && dScore <= -0.25){testResultString = testInfo['b-half']}
-		else if (dScore > -0.25 && dScore < 0.25){testResultString = testInfo['a-b']}
-		else if (dScore >= 0.25 && dScore < 1){testResultString = testInfo['a-half']}
-		else if (dScore >= 1){testResultString = testInfo['a-full']}
+		});
+		if (dScore <= -1) {
+			testResultString = testInfo['b-full'];
+		} else if (dScore > -1 && dScore <= -0.25) {
+			testResultString = testInfo['b-half'];
+		} else if (dScore > -0.25 && dScore < 0.25) {
+			testResultString = testInfo['a-b'];
+		} else if (dScore >= 0.25 && dScore < 1) {
+			testResultString = testInfo['a-half'];
+		} else if (dScore >= 1) {
+			testResultString = testInfo['a-full'];
+		}
 	}
 
 	//Berechne die Standardabweichung
@@ -124,19 +141,27 @@
 
 	function calculateDScore(entries: testEntry[]): number {
 		//TODO: Zählen wie viele falsche oder ungültige Antworten
-		let allTimes:number[] = []
+		let allTimes: number[] = [];
 		// Filtere ungewöhnliche Antwortzeiten und falsche Antworten heraus
 		let validEntries = entries.filter((e) => e.correct && e.time <= 10000);
-		if(validEntries.length/entries.length < 0.75){return -1000} //Error code for to many wrong tries
-		let entriesLessThenMin = entries.filter((e) => e.time < 300)
-		if(entriesLessThenMin.length/entries.length > 0.1){return -1010} //Error code for to fast answers
+		if (validEntries.length / entries.length < 0.75) {
+			return -1000;
+		} //Error code for to many wrong tries
+		let entriesLessThenMin = entries.filter((e) => e.time < 300);
+		if (entriesLessThenMin.length / entries.length > 0.1) {
+			return -1010;
+		} //Error code for to fast answers
 		validEntries.forEach((e) => {
-			if(e.time < 300){e.time = 300}
-			if(e.time > 3000){e.time = 3000}
-			allTimes.push(e.time)
-		})
+			if (e.time < 300) {
+				e.time = 300;
+			}
+			if (e.time > 3000) {
+				e.time = 3000;
+			}
+			allTimes.push(e.time);
+		});
 
-		let standardDeviation = calculateStandardDeviation(allTimes)
+		let standardDeviation = calculateStandardDeviation(allTimes);
 
 		// Teile Einträge in "verknüpft mit A" und "nicht verknüpft mit A"
 		let linkedToAEntries = validEntries.filter((e) => e.aGood);
@@ -151,10 +176,9 @@
 		// Berechne D-Score als Differenz der mittleren Antwortzeiten
 		// Normalisierung des D-Scores durch Teilen durch Standardabweichung
 		// Für die Standardabweichungsberechnung benötigst du weitere Schritte
-		console.log(meanTimeNotLinkedToA, meanTimeLinkedToA, standardDeviation)
-		const dScore = (meanTimeNotLinkedToA- meanTimeLinkedToA) / standardDeviation;
+		console.log(meanTimeNotLinkedToA, meanTimeLinkedToA, standardDeviation);
+		const dScore = (meanTimeNotLinkedToA - meanTimeLinkedToA) / standardDeviation;
 		//let dScore = meanTimeNotLinkedToA - meanTimeLinkedToA;
-
 
 		return dScore;
 	}
@@ -221,7 +245,7 @@
 			curMiddleWord = mixedWords[Math.floor(Math.random() * mixedWords.length)];
 			const isGoodWord = goodJson.includes(curMiddleWord);
 			const isBadWord = badJson.includes(curMiddleWord);
-			if(rounds[curRound - 1].l) {
+			if (rounds[curRound - 1].l) {
 				curAnswer = isGoodWord ? 'left' : isBadWord ? 'right' : '';
 			} else {
 				curAnswer = isGoodWord ? 'right' : isBadWord ? 'left' : '';
@@ -232,7 +256,6 @@
 			const isPic1 = pics1.includes(curPicture);
 			const isPic2 = pics2.includes(curPicture);
 			curAnswer = isPic1 ? 'left' : isPic2 ? 'right' : '';
-
 		} else {
 			curMiddleWord = null;
 			curPicture = null;
@@ -241,7 +264,7 @@
 				curMiddleWord = mixedWords[Math.floor(Math.random() * mixedWords.length)];
 				const isGoodWord = goodJson.includes(curMiddleWord);
 				const isBadWord = badJson.includes(curMiddleWord);
-				if(rounds[curRound - 1].l) {
+				if (rounds[curRound - 1].l) {
 					curAnswer = isGoodWord ? 'left' : isBadWord ? 'right' : '';
 				} else {
 					curAnswer = isGoodWord ? 'right' : isBadWord ? 'left' : '';
@@ -260,23 +283,37 @@
 		inQuestionPause = true;
 		curMiddleWord = null;
 		curPicture = null;
-		setTimeout(function(){
+		setTimeout(function () {
 			newStep();
-			inQuestionPause = false
-		}, 250)
+			inQuestionPause = false;
+		}, 250);
 	}
-
 
 	onMount(async () => {
 		const testInfoResponse = await fetch(testInfoPath);
 		testInfo = await testInfoResponse.json();
 		window.addEventListener('keydown', handleKeyPress);
+		window.addEventListener('click', getMousePosition);
 		await fetchJson();
 		newRound();
 	});
 
+	function getMousePosition(event: MouseEvent) {
+		if (curState == 'test') {
+			const screenWidth = window.innerWidth;
+			const mousePosition = event.clientX;
+			const target = event.target as HTMLElement;
+			const isButton = target.tagName.toLowerCase() === 'button';
+			if (!isButton && mousePosition < screenWidth / 2) {
+				goLeft();
+			} else if (!isButton) {
+				goRight();
+			}
+		}
+	}
+
 	function handleKeyPress(event: KeyboardEvent) {
-		if(curState === 'test') {
+		if (curState === 'test') {
 			if (event.key === 'e') {
 				goLeft();
 			}
@@ -295,14 +332,13 @@
 				testProgress.push(new testEntry(true, rounds[curRound - 1].l));
 				lastTimestamp = Date.now();
 			}
-			wrongAnswer = false
+			wrongAnswer = false;
 			betweenQuestions();
 		} else {
 			if (rounds[curRound - 1].f === 't' && curState === 'test') {
 				testProgress.push(new testEntry(false, rounds[curRound - 1].l));
 			}
-			wrongAnswer = true
-			//TODO: Show some kind of error
+			wrongAnswer = true;
 		}
 	}
 
@@ -316,35 +352,25 @@
 </script>
 
 <main>
-	{#if curState == 'test'}
-	<button on:click={goLeft} class="absolute w-1/2 h-screen">
-
-	</button>
-	<button on:click={goRight} class="absolute right-0 w-1/2 h-screen">
-
-	</button>
-	{/if}
-	<div class="flex h-screen w-screen flex-col items-center justify-center">
-		<div class="flex h-5/6 lg:h-3/5 w-8/12 flex-col items-center">
-			{#if curState == 'pretest'}
-				<DescriptionCard curTestInfo={testInfo} bind:curState bind:curUserAge={userAge} />
-			{:else if curState == 'pause'}
-				<h1 class="m-4 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-					<p>{testInfo.pausen[curRound - 1]}</p>
-					<Button on:click={newRound}>Test fortführen</Button>
-				</h1>
-			{:else if curState == 'test'}
-				<QuestionCard
-					leftWords={curLeftWords}
-					rightWords={curRightWords}
-					middleWord={curMiddleWord}
-					picture={curPicture}
-					error={wrongAnswer}
-				/>
-			{:else if curState == 'end'}
-				<ResultCard result={testResultString} />
-			{/if}
-		</div>
+	<div class="flex h-screen w-screen flex-col items-center justify-center lg:p-24">
+		{#if curState == 'pretest'}
+			<DescriptionCard curTestInfo={testInfo} bind:curState bind:curUserAge={userAge} />
+		{:else if curState == 'pause'}
+			<h1 class="m-4 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+				<p>{testInfo.pausen[curRound - 1]}</p>
+				<Button on:click={newRound}>Test fortführen</Button>
+			</h1>
+		{:else if curState == 'test'}
+			<QuestionCard
+				leftWords={curLeftWords}
+				rightWords={curRightWords}
+				middleWord={curMiddleWord}
+				picture={curPicture}
+				error={wrongAnswer}
+			/>
+		{:else if curState == 'end'}
+			<ResultCard result={testResultString} />
+		{/if}
 	</div>
 </main>
 
